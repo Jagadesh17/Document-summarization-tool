e20fdd9d317bf6e4f9ec47b7c184566d64535af1
JAGADESH R <113334797+Jagadesh17@users.noreply.github.com>
2024-10-01 20:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ import os
 
 app = Flask(__name__)
 
-# Route to render the home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route to handle the file upload and summarization
 @app.route('/summarize', methods=['POST'])
 def summarize():
-    # Handle file upload
+    
     if 'file' in request.files and request.files['file'].filename != '':
         file = request.files['file']
         file_path = os.path.join('uploads', file.filename)
@@ -20,13 +18,11 @@ def summarize():
         summary = summarize_document(file_path)
         return render_template('result.html', summary=summary)
     
-    # Handle text input
     elif 'text' in request.form and request.form['text'].strip() != '':
         input_text = request.form['text']
         summary = summarize_text(input_text)
         return render_template('result.html', summary=summary)
     
-    # No file or text provided
     return "Please upload a file or enter some text."
 
 if __name__ == "__main__":
